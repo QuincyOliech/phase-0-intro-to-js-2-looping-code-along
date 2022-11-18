bd70690c06368dc7891b36f27a1a32cc46de96d5
Linet Oliech <linetaoliech@gmail.com>
2022-11-18 07:25:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const giftGiver=['Guadalupe','Ollie','Aki']
+
+const giftType='surprise'
+
+function writeCards(giftGiver,giftType){
+  const thankYouCards=[]
+  for(let i=0;i<giftGiver.length;i++){
+    thankYouCards.push(`Thank you, ${giftGiver[i]}, for the wonderful ${giftType} gift!`)
+  }
+  return thankYouCards
+}
+
+function countDown(number){
+  while(number>=0){
+    console.log(number)
+    number--
+  }
+}
